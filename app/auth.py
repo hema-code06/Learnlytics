80fa04369abcbaa83b181aafd4190944df78b9c9
hema-code06 <hemavathi.siva06@gmail.com>
2026-02-27 16:28:39 +0530

    Implemented user registration and login endpoints with JWT

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,9 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .. import models, schemas
+from ..database import get_db
+from ..auth import hash_password, verify_password, create_access_token
+
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -5,6 +11,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+router = APIRouter()
 
 SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
@@ -34,3 +41,42 @@ def verify_token(token: str):
         return payload
     except JWTError:
         return None
+
+
+@router.post("/register")
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(
+        models.User.email == user.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    hashed_password = hash_password(user.password)
+
+    new_user = models.User(
+        email=user.email,
+        password_hash=hashed_password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User created successfully.."}
+
+
+@router.post("/login")
+def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(
+        models.User.email == user.email
+    ).first()
+
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Invalid credentials!!")
+    if not verify_password(user.password, db_user.password_hash):
+        raise HTTPException(status_code=400, detail="Invalid credentials!!")
+
+    access_token = create_access_token(
+        data={"sub": str(db_user.id)}
+    )
+    return {"access_token": access_token}
