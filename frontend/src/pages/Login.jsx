d8d86720826fcf12a8a1970f81ad6e864a9f3277
hema-code06 <hemavathi.siva06@gmail.com>
2026-02-27 20:34:17 +0530

    setup frontend login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import API from "../api";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    const res = await API.post("/auth/login", {
+      email,
+      password,
+    });
+
+    localStorage.setItem("token", res.data.access_token);
+    navigate("/dashboard");
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
