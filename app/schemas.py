65d536b6e426eaabc418b654ba45d595fc4167dd
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-08 16:37:56 +0530

    added features in schemas pydantic

--- app/schemas.py
+++ app/schemas.py
@@ -1,13 +1,22 @@
 from pydantic import BaseModel, ConfigDict
 from datetime import date as DateType
-from typing import Optional
+from typing import Optional, List
 from uuid import UUID
 
 
 class LearningEntryBase(BaseModel):
-    date: DateType
-    hours: float
+
     topic: str
+    subtopics: Optional[List[str]] = None
+    from_date: Optional[DateType] = None
+    to_date: Optional[DateType] = None
+    hours: float
+    completed: Optional[bool] = True
+    project_name: Optional[str] = None
+    project_description: Optional[str] = None
+    project_point1: Optional[str] = None
+    project_point2: Optional[str] = None
+    project_point3: Optional[str] = None
 
 
 class LearningEntryCreate(LearningEntryBase):
@@ -15,13 +24,21 @@ class LearningEntryCreate(LearningEntryBase):
 
 
 class LearningEntryUpdate(BaseModel):
-    date: Optional[DateType] = None
-    hours: Optional[float] = None
+
     topic: Optional[str] = None
+    subtopics: Optional[List[str]] = None
+    from_date: Optional[DateType] = None
+    to_date: Optional[DateType] = None
+    hours: Optional[float] = None
+    completed: Optional[bool] = None
+    project_name: Optional[str] = None
+    project_description: Optional[str] = None
+    project_point1: Optional[str] = None
+    project_point2: Optional[str] = None
+    project_point3: Optional[str] = None
 
 
 class LearningEntryResponse(LearningEntryBase):
     id: UUID
 
     model_config = ConfigDict(from_attributes=True)
-
